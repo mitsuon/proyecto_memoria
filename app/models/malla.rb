e0aaf3f6303b62f4d0c5554e2dd0797c079704ea
Pablo <pablo.jc@outlook.com>
2016-08-19 16:35:58 +0000

    fixed malla views  when the association is destroyed

--- app/models/malla.rb
+++ app/models/malla.rb
@@ -1,5 +1,6 @@
 class Malla < ActiveRecord::Base
-    belongs_to :carrera
+    belongs_to :carrera, dependent: :delete
     has_many :modulos
     validates :plan, uniqueness: { scope: :carrera_id, message: " %{value} ya existe" }
+    
 end
